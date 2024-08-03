5ea38ff1fcc613bed53dae0b5fe80e72d824c089
Rob Hasselbaum <rob@hasselbaum.net>
2024-08-03 12:46:58 -0400

    Cleanup

--- hosts/caprica/configuration.nix
+++ hosts/caprica/configuration.nix
@@ -127,9 +127,6 @@
     options = "--delete-older-than 60d";
   };
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
