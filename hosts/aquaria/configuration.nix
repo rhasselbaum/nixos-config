780dd2bdb3d233085fdb0b6f351e11691a03126a
Rob Hasselbaum <rob@hasselbaum.net>
2024-09-28 17:33:12 -0400

    Start WirePlumber/PipeWire at boot and support SSH
    Start WirePlumber and PipeWire at boot. Also, support audio for
    non-interactive logins (SSH) via `audio` group.

--- hosts/aquaria/configuration.nix
+++ hosts/aquaria/configuration.nix
@@ -24,13 +24,6 @@ in
   # Enable Flakes support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable audio devices
-  boot.kernelParams = [ "snd_bcm2835.enable_hdmi=1" "snd_bcm2835.enable_headphones=1" ];
-  boot.loader.raspberryPi.firmwareConfig = ''
-    dtparam=audio=on
-    hdmi_ignore_edid_audio=1
-  '';
-
   # Set your time zone.
   time.timeZone = "America/New_York";
 
@@ -45,6 +38,13 @@ in
     22    # SSH
   ];
 
+  # Enable audio devices
+  boot.kernelParams = [ "snd_bcm2835.enable_hdmi=1" "snd_bcm2835.enable_headphones=1" ];
+  boot.loader.raspberryPi.firmwareConfig = ''
+    dtparam=audio=on
+    hdmi_ignore_edid_audio=1
+  '';
+
   # Pipewire for sound
   security.rtkit.enable = true; # Used to acquire realtime priority if needed
   services.pipewire = {
@@ -53,12 +53,17 @@ in
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    socketActivation = false; # too slow for headless; start at boot instead
   };
+
+  # Start WirePlumber (with PipeWire) at boot.
+  systemd.user.services.wireplumber.wantedBy = [ "default.target" ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."${username}" = {
+    linger = true; # for headless audio at least
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" "networkmanager" "audio" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDVklqPIhhFfoGuTU6hnur2sT8RHUZ5i4KZhmgevBl8Tzc/hcrDYUjylS2LCJbWeMyhldCDjgLjqIAj+X/azYHqT1sRa+xRfx50Q76FX3OxWsmGiYk/u/JRbBw226FpB474gjQkSXPGyjSM4gJB9wfc2nWj8riTa4cPuImMACdDoqFNxIiVWX+nJ9d+YWztOja6spFnP/KoFblfmPvm4tjS6R6OlajRhveeZSmiRrq/GbkOD8HbeXffGltVF8CSf1C1SIuCYZ+p4h7xkYbPyVGQyXfuLDLusWr0+H+1j7uhUT4XCwg3lQCScyGDhhwlTQRwFd7/MxqvED4Q03+I9zqtMeZ6mOHo1/LrncBEJcMVqSfC5tFS4nhvwo6IjhS7TzLkvh5CwAN3/63rNXSivFrfItPWOfE3mkOI7iNFacJqbXNdht+R3jaWYGUh6uCzmn/0YwJ7yuvm1I3Tbpf6A57TJX7RAHI0pU+F81m58xsmHNJQm9QSbPs6W8OPZadkD/Id8JZPkEy7ktdfOy1TzDVZMnGC+JkumYkB2yFF36xu909vPtLZ/Ncu7ffCcPpx9GCsP6KKpWqNTrD4teNta6jn2VKREuxz3Vk5nfJRp8WgBBla/1kUse8tw3LykMiQS6f8uFGb7459dBeDFnbg6ifM9TtYVW31PiKpJgf5z2pWUw== rob@caprica"
     ];
