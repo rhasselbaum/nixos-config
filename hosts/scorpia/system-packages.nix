e16d1608ec621978fc9d9776f5b2055dba170e55
Rob Hasselbaum <rob@hasselbaum.net>
2024-11-17 16:54:20 -0500

    Add Steam to Scorpia

--- hosts/scorpia/system-packages.nix
+++ hosts/scorpia/system-packages.nix
@@ -34,6 +34,15 @@
   ];
   nixpkgs.config.allowUnfree = true;
 
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  };
+
   # Fish
   programs.fish.enable = true;
   programs.bash = {
