8ecc6784137534d8cf53626e5c7774d7606c1451
Rob Hasselbaum <rob@hasselbaum.net>
2025-04-13 16:54:42 -0400

    Reapply "Refactor common system config attibutes into shared module"
    This reverts commit ae00d554caf57f185f015c7a7d988005b5a41a19.

--- hosts/iris/system-packages.nix
+++ hosts/iris/system-packages.nix
@@ -32,19 +32,6 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 
-  # Fish
-  programs.fish.enable = true;
-  programs.bash = {
-    # Have bash interactive shells launch fish. See https://nixos.wiki/wiki/Fish.
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
-
   # KDEConnect
   programs.kdeconnect.enable = true;
 
---
+++ modules/configuration-minimal.nix
@@ -0,0 +1,55 @@
+# Minimal system configuration shared across all hosts.
+
+# Aquaria config
+{ config, lib, pkgs, inputs, ... }:
+{
+  # Enable Flakes support
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Nix Store garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 60d";
+  };
+
+  # Fish
+  programs.fish.enable = true;
+  programs.bash = {
+    # Have bash interactive shells launch fish. See https://nixos.wiki/wiki/Fish.
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
+
