e43f127e8b8b25d3fc878d4affde75ce4cc11693
Rob Hasselbaum <rob@hasselbaum.net>
2025-05-17 19:07:25 -0400

    Sync Picon to Snapcast downstairs group

--- hosts/picon/home-rob-extended.nix
+++ hosts/picon/home-rob-extended.nix
@@ -5,7 +5,7 @@
   programs.snapcast-caprica = {
     enable = true;
     soundcard = "Headphones";
-    latency = 110;
+    latency = 125;
   };
   # Link/unlink Pipewire default audio output device monitor to Snapcast.
   home.file.".mqtt-launcher/launcher.conf.d/20-topics".text = ''
